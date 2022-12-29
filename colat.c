c44661fa6e91e1168562b7c8371281df266a4026
Sebastian LaVine <mail@smlavine.com>
2022-12-28 22:44:40 -0500

    Add braces to if-statement with err() call
    This is a more consistent style.

--- colat.c
+++ colat.c
@@ -217,16 +217,18 @@ main(int argc, char *argv[])
 		}
 	}
 
-	if (SDL_Init(SDL_INIT_VIDEO) != 0)
+	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
 		err("Error initializing SDL: %s", SDL_GetError());
+	}
 	atexit(SDL_Quit);
 
 	window = SDL_CreateWindow("colat",
 		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
 		400, 400,
 		SDL_WINDOW_RESIZABLE);
-	if (window == NULL)
+	if (window == NULL) {
 		err("Error creating window: %s", SDL_GetError());
+	}
 
 	renderer = SDL_CreateRenderer(window, -1,
 		SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
